e82e20c1e015c487ffa814d1da6c7620546f83ee
AnKhanhh <93918878+AnKhanhh@users.noreply.github.com>
2023-06-22 12:44:00 +0700

    refactor

--- src/lexer.c
+++ src/lexer.c
@@ -1,6 +1,15 @@
 #include "lexer.h"
 
 // read input file and print token to output file
+//
+//									_____ skip space
+// flow: check EOF ___ is not? -> /______ check word _____ check keyword
+//							  	  \				 	 \____ is ident
+//								   \_____ check number ___ is not -> illegal (NONE)
+//									\____ check binary symbol
+//									 \___ check unary symbol ___ is not? -> illegal (NONE)
+//
+
 void get_token(FILE *in, FILE *out) {
 	char lexeme[LEXEME_LENGTH], token[TOKEN_LENGTH], c = ' ';
 	token_name t_name;
@@ -8,17 +17,20 @@ void get_token(FILE *in, FILE *out) {
 		token[0] = 0;
 		if ( isspace(c)) {
 			while ( isspace(c)) c = (char) fgetc(in);
+
 		} else if ( isalpha(c)) {
 			charcpy(lexeme, c);
 			int i = 0;
 			for ( ; isalpha(lexeme[i]) || isdigit(lexeme[i]) || lexeme[i] == '_'; lexeme[++i] = (char) fgetc(in)) {}
 			c = lexeme[i];
 			lexeme[i] = 0;
+
 			if (( t_name = is_keyword(lexeme))) {
 				fputs(generate_token(token, lexeme, t_name), out);
 			} else {
 				fputs(generate_token(token, lexeme, IDENT), out);
 			}
+
 		} else if ( isdigit(c)) {
 			charcpy(lexeme, c);
 			int i = 0;
@@ -26,6 +38,7 @@ void get_token(FILE *in, FILE *out) {
 			c = lexeme[i];
 			lexeme[i] = 0;
 			fputs(generate_token(token, lexeme, NUMBER), out);
+
 		} else {
 			lexeme[0] = c;
 			charcpy(lexeme + 1, (char) fgetc(in));
