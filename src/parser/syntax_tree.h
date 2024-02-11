cfa000e29df3bd6d18813988d7f142a5a1d76660
khanh <khanh.da207609@sis.hust.edu.vn>
2024-02-12 04:40:20 +0700

    semantic dcl

--- src/parser/syntax_tree.h
+++ src/parser/syntax_tree.h
@@ -30,7 +30,7 @@ typedef struct Annotation {
 		ETokenType token;
 	} value;
 	enum {
-		ANN_NULL = 0, ANN_IDENT, ANN_NUM, ANN_TOKEN
+		ANN_IDENT = 1, ANN_NUM, ANN_TOKEN
 	} type;
 } Annotation;
 
@@ -44,20 +44,20 @@ typedef struct NodeAST {
 } NodeAST;
 
 //	insert new node to production list, return pointer to topmost node or null if insertion failed
-NodeAST *InsertNode( NodeAST *root, NodeAST *child );
+NodeAST *InsertNode(NodeAST *root, NodeAST *child);
 // create a node
-NodeAST *CreateTreeNode( ESymbolType sb_type, int ann_type, void *ann_ptr );
+NodeAST *CreateTreeNode(ESymbolType sb_type, int ann_type, void *ann_ptr);
 // recursive post-order traversal free()
-void FreeSyntaxTree( NodeAST *root );
+void FreeSyntaxTree(NodeAST *root);
 // print AST to file
-void PrintSyntaxTree( NodeAST *root, FILE *out );
+void PrintSyntaxTree(NodeAST *root, FILE *out);
 
-NodeAST *InsertNode( NodeAST *root, NodeAST *child ) {
-	if( root == NULL || child == NULL) { return child; }
-	else if(root->child_count == 0 ) {
+NodeAST *InsertNode(NodeAST *root, NodeAST *child) {
+	if (root == NULL || child == NULL) { return child; }
+	else if (root->child_count == 0) {
 		root->child_count = PROD_INITIAL_CAPACITY;
-		if(( root->children = calloc(PROD_INITIAL_CAPACITY, sizeof( NodeAST * ))) == NULL) {
-			fprintf(stderr, "PARSER ERR: calloc() failure for %zu bytes\n", PROD_INITIAL_CAPACITY * sizeof( NodeAST * ));
+		if ((root->children = calloc(PROD_INITIAL_CAPACITY, sizeof(NodeAST *))) == NULL) {
+			fprintf(stderr, "PARSER ERR: calloc() failure for %zu bytes\n", PROD_INITIAL_CAPACITY * sizeof(NodeAST *));
 		}
 		root->children[0] = child;
 		return root;
@@ -65,83 +65,83 @@ NodeAST *InsertNode( NodeAST *root, NodeAST *child ) {
 //		child_node->parent = root;
 		int end_index = root->child_count - 1;
 		int i;
-		for( i = end_index; i >= 0; --i ) { if(root->children[i] != NULL) { break; }}
-		if( i < end_index ) {
+		for (i = end_index; i >= 0; --i) { if (root->children[i] != NULL) { break; }}
+		if (i < end_index) {
 			root->children[i + 1] = child;
 		} else {
 			root->child_count += PROD_INITIAL_CAPACITY;
-			root->children = realloc(root->children, root->child_count * sizeof( NodeAST * ));
+			root->children = realloc(root->children, root->child_count * sizeof(NodeAST *));
 			root->children[end_index + 1] = child;
-			for(int j = end_index + 2; j < root->child_count; ++j ) { root->children[j] = NULL; }
+			for (int j = end_index + 2; j < root->child_count; ++j) { root->children[j] = NULL; }
 			return root;
 		}
 	}
 	return NULL;
 }
 
-NodeAST *CreateTreeNode( ESymbolType sb_type, int ann_type, void *ann_ptr ) {
-	NodeAST *new_node = calloc( 1, sizeof( NodeAST ));
+NodeAST *CreateTreeNode(ESymbolType sb_type, int ann_type, void *ann_ptr) {
+	NodeAST *new_node = calloc(1, sizeof(NodeAST));
 	new_node->symbol = sb_type;
-	if( !ann_type ) {
-		assert( ann_ptr == NULL );
+	if (!ann_type) {
+		assert(ann_ptr == NULL);
 		return new_node;
 	}
-	new_node->annotation = calloc( 1, sizeof( Annotation ));
-	if( ann_type == ANN_IDENT ) {
-		strncpy( new_node->annotation->value.ident, ann_ptr, LEXEME_LENGTH );
-	} else if( ann_type == ANN_NUM ) {
+	new_node->annotation = calloc(1, sizeof(Annotation));
+	if (ann_type == ANN_IDENT) {
+		strncpy(new_node->annotation->value.ident, ann_ptr, LEXEME_LENGTH);
+	} else if (ann_type == ANN_NUM) {
 		new_node->annotation->value.number = *(long *) ann_ptr;
-	} else if( ann_type == ANN_TOKEN ) {
+	} else if (ann_type == ANN_TOKEN) {
 		new_node->annotation->value.token = *(ETokenType *) ann_ptr;
 	}
 	new_node->annotation->type = ann_type;
 	return new_node;
 }
 
-void FreeSyntaxTree( NodeAST *root ) {
-	for(int i = 0; i < root->child_count; ++i ) {
-		if(root->children[i] == NULL) { break; }
-		FreeSyntaxTree( root->children[i] );
+void FreeSyntaxTree(NodeAST *root) {
+	for (int i = 0; i < root->child_count; ++i) {
+		if (root->children[i] == NULL) { break; }
+		FreeSyntaxTree(root->children[i]);
 	}
-	free( root->annotation );
-	free( root->children );
-	free( root );
+	free(root->annotation);
+	free(root->children);
+	free(root);
 }
 
-static void SubPrintTree( NodeAST *root, FILE *out, int depth ) {
-	for( int i = 0; i < depth; ++i ) { fputs( "\t", out ); }
-	fprintf( out, "%s", SYMBOL[root->symbol] );
-	if( root->annotation != NULL) {
+static void SubPrintTree(NodeAST *root, FILE *out, int depth) {
+	for (int i = 0; i < depth; ++i) { fputs("\t", out); }
+	fprintf(out, "%s", SYMBOL[root->symbol]);
+	if (root->annotation != NULL) {
 		int type = root->annotation->type;
-		if( type == ANN_IDENT ) {
-			fprintf( out, " : %s", root->annotation->value.ident );
-		} else if( type == ANN_NUM ) {
-			fprintf( out, " : %ld", root->annotation->value.number );
-		} else if( type == ANN_TOKEN ) {
-			fprintf( out, " : %s", TOKENS[root->annotation->value.token] );
+		if (type == ANN_IDENT) {
+			fprintf(out, " : %s", root->annotation->value.ident);
+		} else if (type == ANN_NUM) {
+			fprintf(out, " : %ld", root->annotation->value.number);
+		} else if (type == ANN_TOKEN) {
+			fprintf(out, " : %s", TOKENS[root->annotation->value.token]);
 		} else {
 			fprintf(stderr, "PARSER ERR: invalid node annotation in symbol %s, depth %d \n",
-					SYMBOL[root->symbol], depth );
+					SYMBOL[root->symbol], depth);
 		}
 	}
-	if(root->child_count == 0 ) {
-		assert(root->children == NULL );
-		fputs( "\n", out );
+	if (root->child_count == 0) {
+		assert(root->children == NULL);
+		fputs("\n", out);
 	} else {
-		assert(root->children != NULL );
-		fputs( " {\n", out );
-		for(int i = 0; i < root->child_count; ++i ) {
+		assert(root->children != NULL);
+		fputs(" {\n", out);
+		for (int i = 0; i < root->child_count; ++i) {
 			NodeAST *product_ptr = root->children[i];
-			if( product_ptr == NULL) { break; }
-			SubPrintTree( product_ptr, out, depth + 1 );
+			if (product_ptr == NULL) { break; }
+			SubPrintTree(product_ptr, out, depth + 1);
 		}
-		for( int i = 0; i < depth; ++i ) { fputs( "\t", out ); }
-		fputs( "}\n", out );
+		for (int i = 0; i < depth; ++i) { fputs("\t", out); }
+		fputs("}\n", out);
 	}
 }
 
-void PrintSyntaxTree( NodeAST *root, FILE *out ) {
-	SubPrintTree( root, out, 0 );
+void PrintSyntaxTree(NodeAST *root, FILE *out) {
+	SubPrintTree(root, out, 0);
 }
 
 #endif //COMP_TREE_GEN_H
--- src/semanlyzr/list_semantic_check.h
+++ src/semanlyzr/list_semantic_check.h
@@ -4,17 +4,9 @@
 #include "list_symbol_table.h"
 #include "/parser/syntax_tree.h"
 
-// search for the nearest table entry with the name equals var_name
-// can pass in scope_found to extract the symbol table containing the result
+// search for the nearest table entry with a similar name using strcmp()
+// use scope_found to extract the pointer to the containing symbol table
 static SymbolEntry *search_subroutine(SymbolTable *root, char *var_name, bool search_local_scope_only, SymbolTable **scope_found);
-
-static bool var_dcl_check(NodeAST *node, SymbolTable *root);
-
-static bool arr_dcl_check(NodeAST *node, SymbolTable *root);
-
-static bool const_dcl_check(NodeAST *node, SymbolTable *root);
-
-static bool func_dcl_check(NodeAST *node, SymbolTable *root);
 
 SymbolEntry *SearchLocal(SymbolTable *root, char *var_name, SymbolTable **scope_found) {
 	return search_subroutine(root, var_name, true, scope_found);
@@ -24,11 +16,11 @@ SymbolEntry *SearchGlobal(SymbolTable *root, char *var_name, SymbolTable **scope
 	return search_subroutine(root, var_name, false, scope_found);
 }
 //	except for the program name, all variable, constant, function and array share the same namespace
-//	PL/0 only allow declaration before the main logic, greatly simplify declaration check
-//	return false on error
+//	PL/0 is declarative, meaning it has a separate declaration section, simplifying declaration check
+//	returns false on redeclaration error
 bool OnDeclarationSemantic(NodeAST *node, SymbolTable *root);
 
-bool OnEvocationSemantic(NodeAST *node, SymbolTable *root);
+bool OnInvocationSemantic(NodeAST *node, SymbolTable *root);
 
 bool OnEvaluationSemantic(NodeAST *node, SymbolTable *root);
 
@@ -42,84 +34,60 @@ static SymbolEntry *search_subroutine(SymbolTable *root, char *var_name, bool se
 			*scope_found = root;
 			return &(root->entries[i]);
 		}
-//		recursively move to parent symbol table and search
+//		recursively move to the scope above and search
 	} while (!search_local_scope_only && (root = root->parent) != NULL);
 	return NULL;
 }
 
-static bool var_dcl_check(NodeAST *node, SymbolTable *root) {
-	assert(node->child_count == 0);
-	SymbolTable *result_table = NULL;
-	SymbolEntry *result_entry = NULL;
-//	on name collision
-	if (result_entry == SearchGlobal(root, node->annotation->value.ident, &result_table)) {
-		assert(result_table != NULL);
-		char *ident = node->annotation->value.ident;
-		if (result_table == root) {
-			printf("ERROR: redeclaration of variable in the same scope. \n"
-				   "\t scope: %s, identifier: %s \n",
-				   root->table_name, ident);
-			return false;
-//		allow variable shadowing of same type
-		} else {
-			if (result_entry->type == SB_INT) {
-				printf("WARNING: variable shadowing. \n"
-					   "\t scope: %s, identifier: %s shadowing scope:%s \n",
-					   root->table_name, ident, result_table->table_name);
-			} else {
-				printf("ERROR: redeclaration of variable into a different type. \n"
-					   " \t scope: %s, identifier: %s. from scope: %s, of type %s into %s \n",
-					   root->table_name, ident, result_table->table_name, IDENTTYPE[result_entry->type], IDENTTYPE[SB_INT]);
-				return false;
-			}
-		}
-		return true;
-	}
-}
-
-static bool arr_dcl_check(NodeAST *node, SymbolTable *root) {
-	assert(node->child_count == 2);
-	SymbolTable *result_table = NULL;
-	SymbolEntry *result_entry = NULL;
-	if (result_entry == SearchGlobal(root, node->children[0]->annotation->value.ident, &result_table)) {
-		char *ident = node->children[0]->annotation->value.ident;
-		if (result_table == root) {
-			printf("ERROR: redeclaration of variable in the same scope. \n"
-				   "\t scope: %s, identifier: %s \n",
-				   root->table_name, ident);
-//		allow variable shadowing of same type
-		} else {
-			if (result_entry->type == SB_CONST_INT) {
-				printf("WARNING: variable shadowing. \n"
-					   "\t scope: %s, identifier: %s shadowing scope:%s \n",
-					   root->table_name, ident, result_table->table_name);
-			} else {
-				printf("ERROR: redeclaration of variable into a different type. \n"
-					   " \t scope: %s, identifier: %s. from scope: %s, of type %s into %s \n",
-					   root->table_name, ident, result_table->table_name, IDENTTYPE[result_entry->type], IDENTTYPE[SB_CONST_INT]);
-			}
-		}
-
-	}
-
-}
-
 bool OnDeclarationSemantic(NodeAST *node, SymbolTable *root) {
+	char *ident = NULL;
+	enum EIdentType type = 0;
+	SymbolTable *result_table = NULL;
+	SymbolEntry *result_entry = NULL;
 	switch (node->symbol) {
-//		due to syntax tree structure, var_dcl contains arr_dcl
 		case NAME:
-			return var_dcl_check(node, root);
+			assert(node->children[0] == NULL);
+			ident = node->annotation->value.ident;
+			type = SB_INT;
+			break;
 		case ARR_DCL:
-			return arr_dcl_check(node, root);
-		case CST_DCL:
-			return const_dcl_check(node, root);
+			assert(node->children[0]->symbol == NAME && node->children[1]->symbol == LITERAL);
+			ident = node->children[0]->annotation->value.ident;
+			type = SB_ARRAY;
+			break;
+		case BIN_OP:
+			ident = node->children[0]->annotation->value.ident;
+			type = SB_CONST_INT;
+			break;
 		case PROC_DCL:
-			return func_dcl_check(node, root);
+			assert(node->children[0]->symbol == PROC_ARG);
+			ident = node->annotation->value.ident;
+			type = SB_FUNCTION;
+			break;
 		default:
 			fprintf(stderr,
-					"ERR: declaration semantic check evoked on invalid token. scope: %s", root->table_name);
+					"ERROR: declaration semantic check evoked on invalid token. scope: %s",
+					root->table_name);
 			return false;
 	}
+//	on name collision
+	if (result_entry == SearchGlobal(root, ident, &result_table)) {
+		assert(result_table != NULL);
+//		redeclaration in the same scope is not allowed
+		if (result_table == root) {
+			printf("ERROR: redeclaration of variable in the same scope. \n"
+				   "\t scope: %s, identifier: %s \n",
+				   root->table_name, ident);
+			return false;
+//		variable shadowing is allowed
+		} else {
+			printf("WARNING: variable shadowing. \n"
+				   "\t scope: %s, identifier: %s, type: %s - shadowing scope:%s, type: %s. \n",
+				   root->table_name, ident, SB_IDENT_TYPE[type], result_table->table_name, SB_IDENT_TYPE[result_entry->type]);
+		}
+
+	}
+	return true;
 }
 
 #endif //COMP_LIST_SEMANTIC_CHECK_H
